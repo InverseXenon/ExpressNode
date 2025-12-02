3b855b78dd7057584096463a000aebc829c9de95
Piyush Patil <piyushpatil1741@gmail.com>
2025-12-02 11:39:22 +0530

    Added feed API (In the feed the user can't see their own profile / Any other profile they are in pending state.)

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { userAuth } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 const userRouter = express.Router();
 
 // Get all the pending connection users of the loggedIn user
@@ -19,7 +20,7 @@ userRouter.get("/user/requests/received",userAuth, async (req,res)=>{
     } catch (error) {
         res.status(400).send("Error : " + error);
     }
-})
+});
 
 userRouter.get("/user/connections",userAuth, async(req,res)=>{
     try {
@@ -56,6 +57,35 @@ userRouter.get("/user/connections",userAuth, async(req,res)=>{
         
     } catch (error) {
         res.status(400).send("ERROR : " + error);
+    }
+});
+
+userRouter.get("/user/feed",userAuth,async (req,res)=>{
+    try {
+        loggedInUser = req.user;
+        const connectionRequest = await ConnectionRequest.find({
+            $or:[
+                {fromUserId:loggedInUser._id},
+                {toUserId:loggedInUser._id}
+            ]
+        }).select("fromUserId  toUserId");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequest.forEach(req=>{
+            hideUsersFromFeed.add(req.toUserId.toString());
+            hideUsersFromFeed.add(req.fromUserId.toString());
+        });
+
+        const users = await User.find({
+            $and:[
+                {_id: {$nin: Array.from(hideUsersFromFeed)}},
+                {_id: {$ne: loggedInUser._id}}
+            ]            
+        }).select(["firstName","lastName","photoUrl","age","gender","about","skills"])
+        res.send(users);
+
+    } catch (error) {
+        res.status(400).send("ERROR: " + error);
     }
 })
 
